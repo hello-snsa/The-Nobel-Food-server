f5b689c736f7d027bc1a5706822f815748fe2d10
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-21 10:49:20 +0530

    msg

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -99,7 +99,7 @@ router.get("/", async (req, res) => {
   try {
     // find the post with id.
     console.log("hjhhglkhjvkgflkuglj")
-    const order = await Order.find().populate("userId").populate("receiverId");
+    const order = await Order.find({ "isOrderSelected": false }).populate("userId").populate("receiverId");
 
 
     res.status(200).json(order);
